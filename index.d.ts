1784a33002c032e1a3ffc06e483c70686bd2c160
hackycy <qa894178522@qq.com>
2020-10-05 23:51:22 +0800

    fix：更改d.ts环境声明

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
-import { Validator, ValidatorOptions, ValidationError } from 'class-validator'
+import { Validator, ValidatorOptions } from 'class-validator'
 
 declare module 'egg' {
 
   interface Context {
-    validate: <T>(type: any, data?: any, options?: ValidatorOptions) => Promise<T>
+    validate: <T>(type: any, data?: any, options?: ValidatorOptions) => Promise<T> | Promise<never>
   }
 
   interface Application {
