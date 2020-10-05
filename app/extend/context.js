7d719c1fc8aeea0eb191be95081a5efbb5a34fb2
hackycy <qa894178522@qq.com>
2020-10-05 19:53:48 +0800

    feature：contex上的validate验证完将直接抛出异常

--- app/extend/context.js
+++ app/extend/context.js
@@ -6,12 +6,16 @@ module.exports = {
    * validate data with options
    * @param  {Object} type - validate target, default to `this.request.body`
    * @param  {Object} data - validate target, default to `this.request.body`
-   * @param  {Object} options  - validate options object, see [parameter](https://github.com/typestack/class-validator)
+   * @param  {Object} options  - validate options object, see [class-validator](https://github.com/typestack/class-validator)
    * @return {Promise} Promise<ValidationError[]> - validate errors
    */
-  validate(type, data, options = { forbidUnknownValues: true }) {
+  async validate(type, data, options = { forbidUnknownValues: true }) {
     data = data || this.request.body;
     const instanceCls = plainToClass(type, data);
-    return this.app.validator.validate(instanceCls, options);
+    // return Promise<ValidationError[]>
+    const errors = await this.app.validator.validate(instanceCls, options);
+    if (errors.length > 0) {
+      this.throw(422, 'Validation Failed', { errors });
+    }
   },
 };
