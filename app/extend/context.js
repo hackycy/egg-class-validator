fe20d6109cbb00f1e4e0f3788dec476090d313c4
hackycy <qa894178522@qq.com>
2020-10-10 18:45:00 +0800

    feat：将传递的options更改问assign合并原始配置

--- app/extend/context.js
+++ app/extend/context.js
@@ -11,14 +11,17 @@ module.exports = {
    */
   async validate(type, data, options) {
     data = data || this.request.body;
-    options = options || { validationError: { target: false }, forbidUnknownValues: true, whitelist: true, skipMissingProperties: false, forbidNonWhitelisted: true };
+    const originOptions = { validationError: { target: false }, forbidUnknownValues: true, whitelist: true, skipMissingProperties: false, forbidNonWhitelisted: false };
+    if (options) {
+      Object.assign(originOptions, options);
+    }
     const classTransformOptions =
       this.app.config.classValidator
       && this.app.config.classValidator.classTransformOptions ? this.app.config.classValidator.classTransformOptions : { excludeExtraneousValues: true };
     // 增加转换配置，默认开启类型安全
     const instanceCls = plainToClass(type, data, classTransformOptions);
     // return Promise<ValidationError[]>
-    const errors = await this.app.validator.validate(instanceCls, options);
+    const errors = await this.app.validator.validate(instanceCls, originOptions);
     if (errors.length > 0) {
       if (this.app.config.classValidator && this.app.config.classValidator.handleError && this.app.config.classValidator.handleError instanceof Function) {
         // 如果定义了处理错误的函数，则交给定义的函数进行处理
